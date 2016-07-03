9946f9c0e301127bba6e803b594fc86405498ace
Apoorva Saxena <apsaxena24@gmail.com>
2016-07-03 23:23:58 +0100

    Flash implemented

--- routes/user.js
+++ routes/user.js
@@ -2,24 +2,29 @@ var router = require('express').Router();
 var User = require('../models/user');
 
 router.get('/signup', function(req, res, next){
-  res.render('accounts/signup');
+  res.render('accounts/signup', {
+    errors: req.flash('errors')
+  });
 });
 
 router.post('/signup', function(req, res, next) {
   var user = new User();
 
-  user.profile.name = req.body.email;
+  user.profile.name = req.body.name;
   user.email = req.body.email;
   user.password = req.body.password;
 
   User.findOne( {email: req.body.email}, function(err, existingUser) {
+    console.log(existingUser);
     if(existingUser) {
-      console.log(req.body.email + " is already exists");
+      
+      req.flash('errors', 'Account with that email already exists');
       return res.redirect('/signup');
     } else {
       user.save(function(err, user) {
         if(err) return next(err);
-        res.json("New user has been created");
+
+        return res.redirect('/');
       });
     }
   });
