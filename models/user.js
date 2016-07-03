7e673efea0b1aaa9358f9c25cd4f1b0c99430fc9
Apoorva Saxena <apsaxena24@gmail.com>
2016-07-03 16:58:30 +0100

    Bcrpyt

--- models/user.js
+++ models/user.js
@@ -19,3 +19,17 @@ var UserSchema = new Schema({
     //item: {type: Schema.Type.ObjectId, ref: ''}
   }]
 });
+
+//Hash the password before saving it in the database
+UserSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
