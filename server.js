c4520e0e83d2750f866fcda6eca55428dc90acf8
Apoorva Saxena <apsaxena24@gmail.com>
2016-07-03 19:14:59 +0100

    Postman-connected postman-server-mongolab

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var User = require('./models/user');
+
 var app = express();
 
 mongoose.connect('mongodb://root:secret@ds011705.mlab.com:11705/ecommerce', function(err) {
@@ -14,10 +18,20 @@ mongoose.connect('mongodb://root:secret@ds011705.mlab.com:11705/ecommerce', func
 
 //Middleware
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/', function(req, res){
-  var name = "Apoorva";
-  res.json("Hi my name is "+ name);
+app.post('/create-user', function(req, res, next) {
+  var user = new User();
+
+  user.profile.name = req.body.name;
+  user.password = req.body.password;
+  user.email = req.body.email;
+
+  user.save(function(err) {
+    if(err) next(err);
+    res.json('Successfully created a new user');
+  });
 });
 
 app.listen(3000, function(err) {
